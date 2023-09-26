c602318db1ba6bee7a58b73fbca042220184a41a
Muritala David <davmuri1414@gmail.com>
2023-09-26 09:13:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
+        <h3>Welcome to App React!</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
